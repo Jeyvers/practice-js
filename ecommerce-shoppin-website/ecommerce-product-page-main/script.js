b0b377408fbcae990b2ef165444cdf5b1565017f
Jeyi Adole <erthnice@gmail.com>
2021-11-08 19:32:27 +0100

    Completed Navbar

--- ecommerce-shoppin-website/ecommerce-product-page-main/script.js
+++ ecommerce-shoppin-website/ecommerce-product-page-main/script.js
@@ -2,15 +2,22 @@
 const bars = document.querySelector('.fa-bars');
 const listItemsOverlay = document.querySelector('.list-items-overlay');
 const listItems = document.querySelector('.list-items');
+const closeBtn = document.querySelector('.close-btn');
 
 
 // Classes and functions
-function displayListItems ()  {
+function displayListItems()  {
     listItemsOverlay.classList.add('transparentBcg');
     listItems.classList.add('show-list-items');
-    
+}
+
+function removeListItems() {
+    listItemsOverlay.classList.remove('transparentBcg');
+    listItems.classList.remove('show-list-items');
 }
 
 
 // Event listeners
 bars.addEventListener('click', displayListItems);
+closeBtn.addEventListener('click', removeListItems);
+listItemsOverlay.addEventListener('click', removeListItems)

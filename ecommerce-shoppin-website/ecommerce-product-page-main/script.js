9115ad290de38b020b052bd0f48fdbd1e348509b
Jeyi Adole <erthnice@gmail.com>
2021-11-09 20:34:08 +0100

    Js and Css for mobile version updated

--- ecommerce-shoppin-website/ecommerce-product-page-main/script.js
+++ ecommerce-shoppin-website/ecommerce-product-page-main/script.js
@@ -3,6 +3,16 @@ const bars = document.querySelector('.fa-bars');
 const listItemsOverlay = document.querySelector('.list-items-overlay');
 const listItems = document.querySelector('.list-items');
 const closeBtn = document.querySelector('.close-btn');
+const imgs = document.querySelectorAll('.showcase-img');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
+const viewCart = document.querySelector('#view-cart');
+const cartSection = document.querySelector('.cart-section');
+
+
 
 
 // Classes and functions
@@ -11,13 +21,79 @@ function displayListItems()  {
     listItems.classList.add('show-list-items');
 }
 
-function removeListItems() {
-    listItemsOverlay.classList.remove('transparentBcg');
-    listItems.classList.remove('show-list-items');
+function removeListItems(e) {
+    if(!(e.target.classList.contains('list-items') && (e.target.classList.contains('list-items-overlay')))) {
+        listItemsOverlay.classList.remove('transparentBcg');
+        listItems.classList.remove('show-list-items');
+    }
 }
+
+function showCart() {
+  cartSection.classList.add('transparentBcg');
+} 
+
+const nextImg = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for next slide 
+    if(current.nextElementSibling){
+        // Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        // Add current to start
+        imgs[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for next slide 
+    if(current.previousElementSibling){
+        // Add current to next sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        // Add current to last
+        imgs[imgs.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
 
 
 // Event listeners
 bars.addEventListener('click', displayListItems);
 closeBtn.addEventListener('click', removeListItems);
 listItemsOverlay.addEventListener('click', removeListItems)
+
+
+// Button events 
+next.addEventListener('click', e => {
+    nextImg();
+    if(auto){
+        clearInterval(imgInterval);
+        imgInterval = setInterval(nextImg, intervalTime);
+    }
+});
+
+prev.addEventListener('click', e => {
+    prevImg();
+    if(auto){
+        clearInterval(imgInterval);
+        imgInterval = setInterval(nextImg, intervalTime);
+    }
+});
+
+// Auto Img
+if(auto){
+    // Run next Img at intervalTime tiem
+    imgInterval = setInterval(nextImg, intervalTime);
+}
+viewCart.addEventListener('click', showCart);
+cartSection.addEventListener('click', () => {
+    cartSection.classList.remove('transparentBcg')
+})

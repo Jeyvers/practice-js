7602567fcc825e8e7e8184bb3b05671575a5647c
Jeyi Adole <erthnice@gmail.com>
2021-11-13 03:02:04 +0100

    Refresh

--- ecommerce-shoppin-website/ecommerce-product-page-main/newScript.js
+++ ecommerce-shoppin-website/ecommerce-product-page-main/newScript.js
@@ -40,57 +40,63 @@ class UI {
             result += `
             <article>
             <div id="img-container">
-            <!-- Single collection -->
-           
-            <div class="img-icons">
-            <span class="img-icon" id="next">
-              <i class="fa fa-angle-right" aria-hidden="true"></i>
-            </span>
-            <span class="img-icon" id="prev">
-              <i class="fa fa-angle-left" aria-hidden="true"></i>
-            </span>
-            </div
             <div class="image-area">
-            <div class="imgs">
-                <img src="${product.imgsrc}" class="current showcase-img">
-                <img src="${product.imgalt1}" class="showcase-img">
-                <img src="${product.imgalt2}" class="showcase-img">
-                <img src="${product.imgalt3}" class="showcase-img">
+          <div class="imgs">
+                 <!-- Swiper -->
+            <div class="swiper mySwiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide"> 
+                 <img src="${product.imgsrc}" class="showcase-img">
+                </div>
+                <div class="swiper-slide">
+                 <img src="${product.imgalt1}" class="showcase-img">
+                </div>
+                <div class="swiper-slide">
+                 <img src="${product.imgalt2}" class="showcase-img">
+                </div>
+                <div class="swiper-slide">  
+                 <img src="${product.imgalt3}" class="showcase-img">
+                </div>
+              </div>
               
+              <div class="swiper-button-next"></div> 
+              <div class="swiper-button-prev"></div>
             </div>
-              </div>
-          
-              <div class="image-information-area">
-                <h4>${product.brand}</h4>
-                <h1>${product.title}</h1>
-                <p>
+            
+          </div>
+            </div>
+        
+            <div class="image-information-area">
+              <h4>${product.brand}</h4>
+              <h1>${product.title}</h1>
+              <p>
                 ${product.description}</p>
-          
-                  <h1 class="discount-amount">
-                    $125.00
-                    <span class="discount-percentage">${product.discount}</span>
-                    <span class="amount">${product.price} + '$'</span>
-                  </h1>
-          
-                  <div>
-                  <div class="cart-increase">
-                    <span class="decrement">
-                    <i class="fa fa-minus" aria-hidden="true"></i>
-                    </span>
-                    <span class="num-available">
-                      0
-                    </span>
-                    <span class="increment">
-                      <i class="fa fa-plus" aria-hidden="true"></i>
-                    </span>
-                    </div>
-                    <button class="add-cart-btn" data-id="${product.id}">
-                      <i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart
-                    </button>
+        
+                <h1 class="discount-amount">
+                  $125.00
+                  <span class="discount-percentage">${product.discount}</span>
+                  <span class="amount">${product.price}</span>
+                </h1>
+        
+                <div>
+                <div class="cart-increase">
+                  <span class="decrement">
+                  <i class="fa fa-minus" aria-hidden="true"></i>
+                  </span>
+                  <span class="num-available">
+                    0
+                  </span>
+                  <span class="increment">
+                    <i class="fa fa-plus" aria-hidden="true"></i>
+                  </span>
                   </div>
-          
-              </div>
+                  <button class="add-cart-btn" data-id="${product.id}">
+                    <i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart
+                  </button>
+                </div>
+        
             </div>
+          </div>
             </article>
             `;
         });
@@ -98,41 +104,10 @@ class UI {
     }
 
     listenersDOM() {      
-        // const nexts = [...document.querySelectorAll('#next')];
-        // const prevs = [...document.querySelectorAll('#prev')];
-        // console.log(nexts, prevs)
         // // Event listeners
         bars.addEventListener('click', this.displayListItems);
         closeBtn.addEventListener('click', this.removeListItems);
         document.addEventListener('click', this.removeListItems);
-
-
-        // Button events 
-        // nexts.forEach(next => {
-        //     next.addEventListener('click', e => {
-        //         this.nextImg();
-        //         if(auto){
-        //             clearInterval(imgInterval);
-        //             imgInterval = setInterval(this.nextImg, intervalTime);
-        //         }
-        //     });
-        // });
-
-        // prevs.forEach(prev => {
-        //     prev.addEventListener('click', e => {
-        //         this.prevImg();
-        //         if(auto){
-        //             clearInterval(imgInterval);
-        //             imgInterval = setInterval(this.nextImg, intervalTime);
-        //         }
-        //     });
-        // })
-
-        // // Auto Img
-        // if(auto){
-        //     // Run next Img at intervalTime tiem
-        //     imgInterval = setInterval(this.nextImg, intervalTime);
-        // }
 
         viewCart.addEventListener('click',this.showCart);
         document.addEventListener('click', e => {
@@ -173,41 +148,6 @@ class UI {
         cartOverlay.classList.add('transparentBcg');
     } 
 
-    //  nextImg (){
-    //     const imgs = document.querySelectorAll('.showcase-img');
-      
-    //     // Get current class
-    //     const current = document.querySelector('.current');
-    //     // Remove current class
-    //     current.classList.remove('current');
-    //     // Check for next slide 
-    //     if(current.nextElementSibling){
-    //         // Add current to next sibling
-    //         current.nextElementSibling.classList.add('current');
-    //     } else {
-    //         // Add current to start
-    //         imgs[0].classList.add('current');
-    //     }
-    //     setTimeout(() => current.classList.remove('current'));
-    // };
-
-    // prevImg ()  {
-    //     const imgs = document.querySelectorAll('.showcase-img');
-    //     // Get current class
-    //     const current = document.querySelector('.current');
-    //     // Remove current class
-    //     current.classList.remove('current');
-    //     // Check for next slide 
-    //     if(current.previousElementSibling){
-    //         // Add current to next sibling
-    //         current.previousElementSibling.classList.add('current');
-    //     } else {
-    //         // Add current to last
-    //         imgs[imgs.length - 1].classList.add('current');
-    //     }
-    //     setTimeout(() => current.classList.remove('current'));
-    // };
-
 }
 
 class Storage {
@@ -220,7 +160,6 @@ class Storage {
 document.addEventListener('DOMContentLoaded', () => {
     const ui = new UI();
     const products = new Products();
-    // ui.listenersDOM();
 
     // get all products 
     products.getProducts().then(products => {
@@ -229,5 +168,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }).then(() => {
         ui.getCartButtons();
         ui.listenersDOM();
+        var swiper = new Swiper(".mySwiper", {
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
     }) 
 })
